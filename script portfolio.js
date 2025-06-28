d51ca56d3b428de605869462884484caef648445
helloo-worlddd <uselestings@gmail.com>
2025-06-28 22:45:47 +0530

    Delete script portfolio.js

--- script portfolio.js
+++
@@ -1,8 +0,0 @@
-// Toggle Light/Dark Theme
-const toggleBtn = document.getElementById("toggle-theme");
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
-
-// Update Year in Footer
-document.getElementById("year").textContent = new Date().getFullYear();
